cd0e8bace93c16957f401647eaea81d180fc0d2e
Ebrahim Afridi <afridiebrahimck@gmail.com>
2024-05-21 03:30:37 +0530

    Added: shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,6 @@
-// 'use client'
-// import {
-//   GoogleGenerativeAI,
-//   HarmCategory,
-//   HarmBlockThreshold,
-// } from "@google/generative-ai";
-// import { useState } from "react";
-
-// const apiKey = process.env.GEMINI_API_KEY as string;
-// const genAI = new GoogleGenerativeAI(apiKey);
-
-// const model = genAI.getGenerativeModel({
-//   model: "gemini-1.5-flash-latest",
-// });
-
-// export default function Home() {
-//   const [data, setData] = useState<string>("");
-
-//   const generationConfig = {
-//     temperature: 1,
-//     topP: 0.95,
-//     topK: 64,
-//     maxOutputTokens: 8192,
-//     responseMimeType: "text/plain",
-//   };
-
-//   const safetySettings = [
-//     {
-//       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//     {
-//       category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//     {
-//       category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//     {
-//       category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-//       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-//     },
-//   ];
-
-//   async function run(prompt: string) {
-//     const chatSession = model.startChat({
-//       generationConfig,
-//       safetySettings,
-//       history: [],
-//     });
-
-//     const result = await chatSession.sendMessage(prompt);
-//     console.log(result.response.text());
-//   }
-
-//   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     const prompt = (event.target as HTMLFormElement)?.prompt?.value || "";
-//     run(prompt);
-//   };
-
-//   return (
-//     <main className="flex min-h-screen flex-col items-center p-24">
-//       <form onSubmit={onSubmit} className="">
-//         <p className="mb-2">Enter your prompt here</p>
-//         <input
-//           type="text"
-//           placeholder="Enter your prompt here"
-//           name="prompt"
-//           className="border-none outline-none p-4 rounded-lg text-black"
-//         />{" "}
-//         <br />
-//         <button
-//           type="submit"
-//           className="bg-white border border-none outline-none p-4 rounded-lg text-black font-bold uppercase mt-2"
-//         >
-//           Submit
-//         </button>
-//       </form>
-//       {data && (
-//         <div>
-//           <h1 className="mt-32">Output</h1>
-//           <div dangerouslySetInnerHTML={{ __html: data }} />
-//         </div>
-//       )}
-//     </main>
-//   );
-// }
-
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   GoogleGenerativeAI,
   HarmCategory,
@@ -164,19 +76,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
       <form onSubmit={onSubmit} className="">
         <p className="mb-2">Enter your prompt here</p>
-        <input
-          type="text"
-          placeholder="Enter your prompt here"
-          name="prompt"
-          className="border-none outline-none p-4 rounded-lg text-black"
-        />{" "}
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input
+            type="text"
+            placeholder="Enter your prompt here"
+            name="prompt"
+          />
+          <Button type="submit">
+            Submit
+          </Button>
+        </div>
         <br />
-        <button
-          type="submit"
-          className="bg-white border border-none outline-none p-4 rounded-lg text-black font-bold uppercase mt-2"
-        >
-          Submit
-        </button>
       </form>
       {data && (
         <div>
